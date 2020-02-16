1af1854e9c7023eff2806dd272840a40ddc05369
Tomasz Szuster <tomasz.szuster@gmail.com>
2020-02-16 15:17:11 +0100

    :sparkles: Utilize lsblk to get partition types :recycle: Refactoring code for unmounting partitions

--- pysfdisk/block_device.py
+++ pysfdisk/block_device.py
@@ -33,7 +33,6 @@ def find_executable(name: str) -> Union[str, None]:
     :return: Return the string representation of the path with forward (/) slashes.
 
     """
-
     standard_executable_paths = ["/bin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
 
     for path in standard_executable_paths:
@@ -49,10 +48,10 @@ class BlockDevice:
 
     SFDISK_EXECUTABLE = find_executable(name="sfdisk")
     DD_EXECUTABLE = find_executable(name="dd")
+    LSBLK_EXECUTABLE = find_executable(name="lsblk")
 
     def __init__(self, path, use_sudo=False):
-        """Set member variables, perform checks and obtain the initial
-        partition table."""
+        """Set member variables, perform checks and obtain the initial partition table."""
         # Setup member variables
         self.path = path
         self.use_sudo = use_sudo
@@ -63,6 +62,7 @@ class BlockDevice:
         self._assert_root()
         self._ensure_exists()
         self._read_partition_table()
+        self._umount_partitions()
 
     def get_partitions(self):
         """Return the partition objects for the block object."""
@@ -85,12 +85,46 @@ class BlockDevice:
         :return: output of dd command
 
         """
-
-        command_list = ["dd", f"if={self.path}", f"of={destination_file}", "bs=512", "count=1"]
+        command_list = [self.DD_EXECUTABLE, f"if={self.path}", f"of={destination_file}", "bs=512", "count=1"]
         if self.use_sudo:
             command_list.insert(0, "sudo")
         save_mbr = subprocess.run(command_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
         return save_mbr.check_returncode()
+
+    def _umount_partitions(self) -> None:
+        """
+        Umount mounted partition to allow it to be processed by partclone or dd.
+
+        :return: return code from the dd command
+
+        """
+        partition_list = self.get_fs_types()
+
+        for key, value in partition_list.items():
+            command_list = ["umount", f"/dev/{key}"]
+            if self.use_sudo:
+                command_list.insert(0, "sudo")
+            subprocess.run(command_list, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
+
+    def get_fs_types(self):
+        disk_name = self.path.split("/")[2]
+        fs_types = {}
+
+        command_list = [self.LSBLK_EXECUTABLE, "-o", "NAME,TYPE,FSTYPE", "-b", "-J"]
+        if self.use_sudo:
+            command_list.insert(0, "sudo")
+        command_output = json.loads(subprocess.check_output(command_list, shell=False))
+        all_disks = command_output.get("blockdevices")
+
+        for disk in all_disks:
+            if disk.get("name") == disk_name:
+                all_disks = disk.get("children")
+
+        for _ in all_disks:
+            if _.get("fstype"):
+                fs_types[_.get("name")] = _.get("fstype")
+
+        return fs_types
 
     def _read_partition_table(self):
         """Create the partition table using sfdisk and load partitions."""
@@ -110,7 +144,6 @@ class BlockDevice:
             raise BlockDeviceDoesNotExist("Block device %s does not exist" % self.path)
 
     def _assert_root(self):
-        """Ensure that the script is being run as root, or 'as root' has been
-        specified."""
+        """Ensure that the script is being run as root, or 'as root' has been specified."""
         if os.getuid() != 0 and not self.use_sudo:
             raise NotRunningAsRoot("Must be running as root or specify to use sudo")
