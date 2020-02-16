19f0a67f2f5d071bb5de48f9a22b3051863ee5d2
Tomasz Szuster <tomasz.szuster@gmail.com>
2020-02-16 02:11:30 +0100

    :recycle: Refactoring code in setup.py

--- setup.py
+++ setup.py
@@ -21,15 +21,15 @@ from setuptools import setup, find_packages
 
 import pysfdisk
 
-with open('VERSION', 'r') as version_fh:
+with open("VERSION", "r") as version_fh:
     version = version_fh.read()
 
 setup(
-    name='pysfdisk',
+    name="pysfdisk",
     version=version,
-    license='GNU GENERAL PUBLIC LICENSE',
-    author='Matt Comben',
-    platforms='any',
-    author_email='matthew@dockstudios.co.uk',
-    packages=list(find_packages())
+    license="GNU GENERAL PUBLIC LICENSE",
+    author="Matt Comben",
+    platforms="any",
+    author_email="matthew@dockstudios.co.uk",
+    packages=list(find_packages()),
 )
